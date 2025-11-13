dc7ef796a7a7238f420f7aa3d9d28d3f3957a1e6
Sebastian <zain-wave@outlook.com>
2025-11-12 21:25:14 -0500

    Bandera Booleana

--- 1_Etapa.py
+++ 1_Etapa.py
@@ -1,44 +1,40 @@
-while True:
+edad_valida = False
+while not edad_valida:
     if not (edad_str := input("Ingresa tu edad: ").strip()):
         print("Debes ingresar algo")
         continue
     
-    if edad_str.isdigit():
+    if edad_str.lstrip('-').isdigit(): 
         edad = int(edad_str)
         if edad < 0 or edad > 120: 
             print("Ingresa una edad valida")
             continue
-        break
+        edad_valida = True
     else:
         print("Ingresa un numero entero")
 
-
-while True:
+nivel_valido = False
+while not nivel_valido:
     if not (nivel := input("¿Actualmente estudias, trabajas o ninguna? ").strip().lower()):
         print("Debes ingresar algo")
         continue
     
     if nivel in ["estudia", "trabaja", "ninguna"]:
         nivel = nivel.capitalize()
-        break
+        nivel_valido = True
     else:
         print("Responde solo con: Estudia, Trabaja o Ninguna")
 
 if edad < 6:
     etapa = "Infante"
-
 elif (edad >= 6 and edad <= 17) and (nivel == "Estudia"):
     etapa = "Estudiante Escolar"
-
 elif (edad >= 18 and edad <= 25) and (nivel == "Estudia"):
     etapa = "Universitario"
-
 elif (edad > 25) and (nivel == "Trabaja"):
     etapa = "Adulto activo"
-
 elif (edad > 60) and (nivel == "Ninguna"):
     etapa = "Adulto mayor jubilado"
-
 else:
     etapa = "No determinado"
 
